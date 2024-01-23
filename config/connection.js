4e8ebc308c6ee79af4fbccf29c9a55f3e093fc13
lifewawj <arevaloaj01@gmail.com>
2024-01-23 14:23:17 -0800

    ADDED POST and GET Routes for the Category Model

--- config/connection.js
+++ config/connection.js
@@ -4,11 +4,11 @@ const Sequelize = require('sequelize');
 
 const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
-  
+
   : new Sequelize(
     // Access the .env SECRET file and grabs the variable named DB_NAME
     process.env.DB_NAME, 
-    // Access the .env SECRET file and grabs the variable named DB_NAME
+    // Access the .env SECRET file and grabs the variable named DB_USER
     process.env.DB_USER, 
     // Access the .env SECRET file and grabs the variable named DB_PASSWORD
     process.env.DB_PASSWORD, {
--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -3,26 +3,70 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
-  // find all categories
-  // be sure to include its associated Products
+// find all categories
+// be sure to include its associated Products
+router.get('/', async (req, res) => {
+  try {
+
+    const categoryData = await Category.findAll({
+      include: [{ model: Product }]
+    });
+
+    res.status(200).json(categoryData);
+
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.get('/:id', (req, res) => {
-  // find one category by its `id` value
-  // be sure to include its associated Products
+// find one category by its `id` value
+// be sure to include its associated Products
+router.get('/:id', async (req, res) => {
+  try {
+
+    const categoryData = await Category.findByPk(req.params.id, {
+      include: [{ model: Product }]
+    });
+
+    res.status(200).json(categoryData);
+
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.post('/', (req, res) => {
-  // create a new category
+// create a new category
+router.post('/', async (req, res) => {
+  try {
+
+    const categoryData = await Category.create(req.body);
+
+    res.json(categoryData)
+
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
+  try {
+
+    
+
+
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
+  try {
+
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
